84ebb8444f8179cd2ee9428dc22c52bbce275d12
Javid H. Atakishiyev <JavidHa@CODE.LOCAL>
2019-10-31 15:59:30 +0400

    some shity changes

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: false,
+    items: 4
+  });
+  $("#lang").click(function () {
+    let display = $(".options").css("display");
+    if (display == "none") {
+      $(".options").css("display", "block");
+    } else {
+      $(".options").css("display", "none");
+    }
+  });
+  $("#USD").click(function () {
+    alert("ok");
+  });
+  $("#EUR").click(function () {
+    alert("ok");
+  });
+//   $(document).scroll(function () {
+//     if (
+//       document.body.scrollTop > 80 ||
+//       document.documentElement.scrollTop > 80
+//     ) {
+//       $(".top-heading").addClass("d-none");
+//     } else {
+//       $(".top-heading").removeClass("d-none");
+//     }
+//   });
+});
