645b8f8d09ced5c30b5749ada0dc917e55d5d271
bonoboisamonkey <bonoboisamonkey@github.com>
2019-11-03 00:59:38 +0400

    minor changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,6 @@
 
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    dots: false,
-    items: 4
-  });
+
   $("#lang").click(function () {
     let display = $(".options").css("display");
     if (display == "none") {
@@ -22,12 +17,36 @@ $(document).ready(function () {
   });
   $(document).scroll(function () {
     if (
-      document.body.scrollTop > 80 ||
-      document.documentElement.scrollTop > 80
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
     ) {
       $(".top-heading").addClass("d-none");
+      $("#button2top").removeClass("d-none");
+
     } else {
       $(".top-heading").removeClass("d-none");
+      $("#button2top").addClass("d-none");
     }
   });
+
+  $("#button2top").click(function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  })
+
+  $(".image-container").hover(function () {
+    $(".image-overlay", this).css({ "diplay": "block", "opacity": "1" });
+    $(".image-overlay button", this).addClass("animated fadeInUp ")
+  }, function () {
+    $(".image-overlay", this).css({ "diplay": "none", "opacity": "0" });
+    $(".image-overlay button", this).removeClass("animated fadeInUp")
+  })
+
+  $(".block-items").hover(function () {
+    $(".overlay", this).css({ "diplay": "block", "opacity": "1" });
+    $(".overlay div", this).addClass("animated fadeInUp ")
+  }, function () {
+    $(".overlay", this).css({ "diplay": "none", "opacity": "0" });
+    $(".overlay div", this).removeClass("animated fadeInUp")
+  })
 });
